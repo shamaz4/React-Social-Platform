c14f12dfb3200be17243caf0e1714711e68a3928
shamaz aamir <qasimnawaz751@gmail.com>
2023-02-24 10:22:28 +0500

    first commit

---
+++ src/components/infoSection/infoSection.jsx
@@ -0,0 +1,26 @@
+import React,{Fragment} from 'react'
+import './infoSection.css'
+
+function InfoSection() {
+    return (
+        <Fragment>
+
+            <div className="infoContainer">
+                
+                <img src='./images/pp1.png' alt='' className="infoImage"/>
+                
+                <div className="infoContent">
+                
+                    <div className="infoUsername">Shamaz_officials</div>
+                
+                    <div className="infoDescription">Shamaz</div>
+                
+                </div>
+            
+            </div>
+
+        </Fragment>
+    )
+}
+
+export default InfoSection
